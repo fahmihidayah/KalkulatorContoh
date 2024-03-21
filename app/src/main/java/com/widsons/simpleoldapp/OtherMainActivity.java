2cb0e5e57fbb4065155deb85a7c9850fed7aceb0
fahmi-mac <f.hidayah@pixilapps.com>
2024-03-21 22:03:08 +0700

    Add Implementation Tambah

---
+++ app/src/main/java/com/widsons/simpleoldapp/OtherMainActivity.java
@@ -0,0 +1,24 @@
+package com.widsons.simpleoldapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.widsons.simpleoldapp.databinding.ActivityOtherMainBinding;
+
+public class OtherMainActivity extends AppCompatActivity {
+    ActivityOtherMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_other_main);
+    }
+
+    public void onButtonTambahClick() {
+        int angkaInt1 = Integer.parseInt(binding.editText1.getText().toString());
+        int angkaInt2 = Integer.parseInt(binding.editText2.getText().toString());
+        binding.textViewHasil.setText("" + (angkaInt1 + angkaInt2));
+    }
+}
