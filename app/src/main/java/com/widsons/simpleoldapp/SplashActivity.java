f957430460549f695d279fcce22fc7177bbff3bc
fahmi-mac <f.hidayah@pixilapps.com>
2024-03-22 07:43:21 +0700

    update activity

--- app/src/main/java/com/widsons/simpleoldapp/SplashActivity.java
+++ app/src/main/java/com/widsons/simpleoldapp/SplashActivity.java
@@ -2,15 +2,13 @@ package com.widsons.simpleoldapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        startActivity(new Intent("com.widsons.simpleoldapp.SplashActivity"));
+        setContentView(R.layout.activity_splash);
     }
 }
